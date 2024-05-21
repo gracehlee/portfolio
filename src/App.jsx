dd3e92e5d8b7df71202a6a70c8f9287c19c58e3b
Grace <gracehlee.070@gmail.com>
2024-05-21 10:58:14 -0700

    style: nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './css/index.css';
+import 'bootstrap/dist/css/bootstrap.css'
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,42 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Ensure Bootstrap JS is included
+import '../css/index.css';
+
 export default function Nav() {
     return (
         <header>
-            <nav>
-                <div>
-                    <p className='name'>grace lee</p>
-                    <p>Hello Nav!</p>
+            <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: 'var(--pink)' }}>
+                <div className="container-fluid">
+                    <a className="name" href="#">grace lee</a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse sticky-top" id="navbarNav">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <a className="nav-link nav-content" href="#">about</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link nav-content" href="#">projects</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link nav-content" href="#">contact</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link nav-content" href="#">resume</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </header>
-    )
+    );
 }
