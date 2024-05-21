f5f466c04060fedc055d1dfc0b61b7b51a46429a
Grace <gracehlee.070@gmail.com>
2024-05-20 20:40:41 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        {/* Nav */}
+        <Routes>
+          <Route></Route>
+        </Routes>
+        {/* Footer */}
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+function Home(props) {
+    return (
+        <main>
+            <div>
+                <p>Hello world!</p>
+            </div>
+        </main>
+    )
+}
+
+export default Home
