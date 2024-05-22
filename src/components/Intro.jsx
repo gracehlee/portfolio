edec55a849b40ab782c20f0eb66d7f8ef4406535
Grace Lee <gracehlee.070@gmail.com>
2024-05-22 01:10:31 +0000

    Navigation

---
+++ src/components/Intro.jsx
@@ -0,0 +1,45 @@
+import profile from '../images/transparent.png';
+import '../css/index.css';
+import linkedin from '../images/linkedin.png'
+import gitlab from '../images/gitlab.png'
+import gmail from '../images/gmail.png'
+import { useState } from 'react'
+
+export default function Intro() {
+    const handleCopy = () => {
+        const my_email = 'ghlee.contactme@gmail.com'
+        navigator.clipboard.writeText(my_email)
+        window.alert('Email copied to clipboard!')
+    }
+
+    return (
+        <div className="c-row">
+            <div className="d-flex align-items-center">
+                <div className="about-text align-items-center">
+                    <h2 className="name">grace lee</h2>
+                    <h4 style={{color: 'navy'}}>FULL STACK ENGINEER</h4>
+                    <div><br /></div>
+                    <a
+                        href="https://www.linkedin.com/in/haeungracelee/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={linkedin} alt="linkedin" width='40px'style={{paddingRight: '10px', cursor: 'pointer'}}/>
+                    </a>
+                    <a
+                        href="https://gitlab.com/gracehlee"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={gitlab} alt="gitlab" width='40px' style={{paddingRight: '10px', cursor: 'pointer'}}/>
+                    </a>
+                    <img src={gmail} alt="gmail" width='45px'style={{paddingRight: '10px', cursor: 'pointer'}} onClick={handleCopy}/>
+                    <div><br /></div>
+                </div>
+                <div className="profile-image align-items-center">
+                    <img src={profile} alt="Profile" />
+                </div>
+            </div>
+        </div>
+    );
+}
