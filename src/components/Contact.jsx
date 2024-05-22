de4d538f246d097f4305a45293fdf170f34d6ab8
Grace <gracehlee.070@gmail.com>
2024-05-21 22:39:11 -0700

    feat: install EmailJS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,17 @@
+import code from '../images/code2.png'
+
 export default function Contact() {
     return (
-        <div className="contact">
+        <div
+            className="contact"
+            style={{
+                backgroundImage: `url(${code})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundPositionY: 'fixed',
+                backgroundAttachment: 'fixed',
+            }}
+        >
             <h1 style={{fontWeight: 'bold'}}>Contact</h1>
             <div><br /></div>
             <p>Hello World</p>
